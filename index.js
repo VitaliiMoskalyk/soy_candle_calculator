e5cd7cc85138ebdd9a8c91fdb8322aa261fddb73
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2025-02-23 18:26:20 +0200

    update
    add amount of jars in the calculator

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jar=document.getElementById("jar");
+const jar_amount=document.getElementById("jar-amount");
 const intense=document.getElementById("intense");
 const s=document.getElementById("s");
 const calculate=document.getElementById("calculate");
@@ -13,15 +14,15 @@ const isInAverage=(el)=>{
  calculate.addEventListener('click',()=>getCalculatedData());
 
  const getCalculatedData=()=>{
-if(isInAverage(jar)&&isInAverage(intense)&&isInAverage(s)){
+if(isInAverage(jar)&&isInAverage(intense)&&isInAverage(s)&&isInAverage(jar_amount)){
     table.style.visibility='visible';
-    document.getElementById('jar-value').textContent=parseFloat(jar.value).toFixed(2);
-    document.getElementById('intense-value').textContent=parseFloat(intense.value).toFixed(2);
+    document.getElementById('jar-value').textContent=parseFloat(jar.value).toFixed(1)+" мл"+" * "+jar_amount.value+" шт";
+    document.getElementById('intense-value').textContent=parseFloat(intense.value).toFixed(2) +" %";
     document.getElementById('s-value').textContent=parseFloat(s.value).toFixed(3);
 
-    document.getElementById('v-value').textContent=((jar.value*(1-intense.value/100))*0.86*intense.value/100/s.value).toFixed(2);
-    document.getElementById('m-value').textContent=((jar.value*(1-intense.value/100))*0.86*intense.value/100).toFixed(2);
-    document.getElementById('wax-value').textContent=((jar.value*(1-intense.value/100))*0.86).toFixed(2);
+    document.getElementById('v-value').textContent=(((jar.value*jar_amount.value*(1-intense.value/100))*0.86*intense.value/100/s.value)).toFixed(2) + " мл";
+    document.getElementById('m-value').textContent=(((jar.value*(1-intense.value/100))*0.86*intense.value/100)*jar_amount.value).toFixed(2)+" г";
+    document.getElementById('wax-value').textContent=(((jar.value*(1-intense.value/100))*0.86)*jar_amount.value).toFixed(2)+" г";
 }
 else{
 table.style.visibility='hidden'
