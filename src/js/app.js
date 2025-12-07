07c26d33bc52cd7ce4877b60e5c1eeaa3eccbe81
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2025-12-07 21:40:50 +0200

    small fixes

--- src/js/app.js
+++ src/js/app.js
@@ -46,7 +46,9 @@ const db = getFirestore();
   const form = document.getElementById("form");
   const calculate = document.getElementById("calculate");
   const authToastEl = document.getElementById("authToast");
+  const toastText=document.getElementById('toast-body')
   const authToast = authToastEl ? new bootstrap.Toast(authToastEl) : null;
+  const tableBody = document.querySelector("#historyTable tbody");
 
   let userLoggedIn = false;
 
@@ -129,36 +131,7 @@ await setDoc(doc(db, "users", user.uid), {
     })
   );
 
-//   function getCalculatedData() {
-//     const jar = document.getElementById("jar");
-//     const jar_amount = document.getElementById("jar-amount");
-//     const intense = document.getElementById("intense");
-//     const s = document.getElementById("s");
-//     const waxGravity = document.getElementById("floatingSelectGrid");
 
-//     const isInRange = (el) =>
-//       parseFloat(el.value) >= parseFloat(el.min) && parseFloat(el.value) <= parseFloat(el.max);
-
-//     if ([jar, jar_amount, intense, s].every(isInRange)) {
-//       const V_c = parseFloat(jar.value);
-//       const jarCount = parseFloat(jar_amount.value);
-//       const f = parseFloat(intense.value) / 100;
-//       const rho_w = parseFloat(waxGravity.value);
-//       const rho_f = parseFloat(s.value);
-
-//       const V_total = V_c * jarCount;
-//       const m_w_total = V_total / ((1 / rho_w) + (f / rho_f));
-//       const m_f_total = f * m_w_total;
-//       const V_f_total = m_f_total / rho_f;
-
-//       document.getElementById("jar-value").textContent = `${V_c} мл * ${jarCount} шт`;
-//       document.getElementById("intense-value").textContent = `${(f * 100).toFixed(2)} %`;
-//       document.getElementById("s-value").textContent = rho_f.toFixed(3);
-//       document.getElementById("v-value").textContent = V_f_total.toFixed(2) + " мл";
-//       document.getElementById("m-value").textContent = m_f_total.toFixed(2) + " г";
-//       document.getElementById("wax-value").textContent = m_w_total.toFixed(2) + " г";
-//     }
-//   }
 
   // === Кнопка "Розрахувати" ===
  
@@ -167,8 +140,7 @@ await setDoc(doc(db, "users", user.uid), {
     if (!validateForm()) return;
 
     if (!userLoggedIn) {
-      console.dir(authToast)
-      authToast._element.innerHTML='<div class="d-flex"><div class="toast-body">       Для виконання розрахунку потрібно увійти в систему.</div> <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>  </div>';
+      toastText.textContent=`Для виконання розрахунку потрібно увійти в систему.`;
       authToast?.show()
     } else {
        result=getCalculatedData();
@@ -199,7 +171,7 @@ await setDoc(doc(db, "users", user.uid), {
             { merge: true }
         );
 
-        authToast._element.innerHTML='<div class="d-flex"><div class="toast-body">       Розрахунок збережено в профілі. </div> <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>  </div>';
+      toastText.textContent=`Розрахунок збережено`;
       authToast?.show()
         });
             
@@ -210,7 +182,7 @@ await setDoc(doc(db, "users", user.uid), {
 
 
 
-  const tableBody = document.querySelector("#historyTable tbody");
+  
 
 onAuthStateChanged(auth, async (user) => {
   if (!user) return;
@@ -236,7 +208,7 @@ function renderTable(history) {
     const row = document.createElement("tr");
     row.innerHTML = `
       <td>${index + 1}</td>
-      <td  data-field="jarValue">${r.jarValue}мл,${r.jarAmount}шт</td>
+      <td  data-field="jarValue">${r.jarValue}мл\n${r.jarAmount}шт</td>
       <td  data-field="waxType">${r.waxType}</td>
       <td  data-field="intense">${r.intense}%</td>
       <td contenteditable="true" data-field="scent">${r.scent?r.scent:r.intense}</td>
@@ -276,6 +248,8 @@ async function deleteRow(e) {
 
   await updateDoc(ref, { history: data.history });
   renderTable(data.history);
+      toastText.textContent=`Рядок ${index+1} видалено`;
+      authToast?.show()
 }
 
 async function saveEditedCell(e) {
--- src/js/modules/getCalculatedData.js
+++ src/js/modules/getCalculatedData.js
@@ -35,3 +35,35 @@ export function getCalculatedData() {
     }
     return null;
   }
+
+
+  //   function getCalculatedData() {
+  //     const jar = document.getElementById("jar");
+  //     const jar_amount = document.getElementById("jar-amount");
+  //     const intense = document.getElementById("intense");
+  //     const s = document.getElementById("s");
+  //     const waxGravity = document.getElementById("floatingSelectGrid");
+  
+  //     const isInRange = (el) =>
+  //       parseFloat(el.value) >= parseFloat(el.min) && parseFloat(el.value) <= parseFloat(el.max);
+  
+  //     if ([jar, jar_amount, intense, s].every(isInRange)) {
+  //       const V_c = parseFloat(jar.value);
+  //       const jarCount = parseFloat(jar_amount.value);
+  //       const f = parseFloat(intense.value) / 100;
+  //       const rho_w = parseFloat(waxGravity.value);
+  //       const rho_f = parseFloat(s.value);
+  
+  //       const V_total = V_c * jarCount;
+  //       const m_w_total = V_total / ((1 / rho_w) + (f / rho_f));
+  //       const m_f_total = f * m_w_total;
+  //       const V_f_total = m_f_total / rho_f;
+  
+  //       document.getElementById("jar-value").textContent = `${V_c} мл * ${jarCount} шт`;
+  //       document.getElementById("intense-value").textContent = `${(f * 100).toFixed(2)} %`;
+  //       document.getElementById("s-value").textContent = rho_f.toFixed(3);
+  //       document.getElementById("v-value").textContent = V_f_total.toFixed(2) + " мл";
+  //       document.getElementById("m-value").textContent = m_f_total.toFixed(2) + " г";
+  //       document.getElementById("wax-value").textContent = m_w_total.toFixed(2) + " г";
+  //     }
+  //   }
