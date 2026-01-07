6594e02650b95674e1f1ac29a6bf0a442b7d032e
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2026-01-07 13:16:14 +0200

    add picture

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,52 +1,44 @@
 import webp from "gulp-webp";
 import imagemin from "gulp-imagemin";
+import mozjpeg from "imagemin-mozjpeg";
+import optipng from "imagemin-optipng";
+import svgo from "imagemin-svgo";
 
 export const images = () => {
-	return app.gulp.src(app.path.src.images)
-		.pipe(app.plugins.plumber(
-			app.plugins.notify.onError({
-				title: "IMAGES",
-				message: "Error: <%= error.message %>"
-			}))
-		)
-		.pipe(app.plugins.newer(app.path.build.images))
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				webp()
-			)
-		)
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				app.gulp.dest(app.path.build.images)
-			)
-		)
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				app.gulp.src(app.path.src.images)
-			)
-		)
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				app.plugins.newer(app.path.build.images)
-			)
-		)
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				imagemin({
-					progressive: true,
-					svgoPlugins: [{ removeViewBox: false }],
-					interlaced: true,
-					optimizationLevel: 3 // 0 to 7
-				})
-			)
-		)
-		.pipe(app.gulp.dest(app.path.build.images))
-		.pipe(app.gulp.src(app.path.src.svg))
-		.pipe(app.gulp.dest(app.path.build.images))
-		.pipe(app.plugins.browsersync.stream());
-}
+  return app.gulp
+    .src("src/img/**/*.{jpg,jpeg,png,svg}", {
+      base: "src",
+      encoding: false
+    })
+    .pipe(app.plugins.plumber())
+
+    // WEBP (build only)
+    .pipe(app.plugins.if(app.isBuild, webp({ quality: 85 })))
+    .pipe(app.plugins.if(app.isBuild, app.gulp.dest("dist")))
+
+    // ORIGINALS + OPTIMIZATION
+    .pipe(app.plugins.if(
+      app.isBuild,
+      imagemin([
+        mozjpeg({ quality: 80, progressive: true }),
+        optipng({ optimizationLevel: 5 }),
+        svgo({ plugins: [{ removeViewBox: false }] })
+      ])
+    ))
+
+    .pipe(app.gulp.dest("dist"))
+    .pipe(app.plugins.browsersync.stream());
+};
+
+
+// export const images = () => {
+//   return app.gulp.src('src/img/**/*.{jpg,jpeg,png,svg}', { base: 'src', allowEmpty: true,encoding: false })
+//     .pipe(app.plugins.plumber(
+//       app.plugins.notify.onError({
+//         title: 'IMAGES',
+//         message: 'Error: <%= error.message %>'
+//       })
+//     ))
+//     .pipe(app.gulp.dest('dist'))
+//     .pipe(app.plugins.browsersync.stream());
+// };
